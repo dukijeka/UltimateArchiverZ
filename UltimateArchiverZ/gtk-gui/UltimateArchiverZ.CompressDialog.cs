65b6065d6fe4c3c935b227e620fac01408267012
Unknown <dukijeka@gmail.com>
2018-04-14 03:29:42 +0200

    Design fix
    removed ugly white widget, which had no purpose

--- UltimateArchiverZ/gtk-gui/UltimateArchiverZ.CompressDialog.cs
+++ UltimateArchiverZ/gtk-gui/UltimateArchiverZ.CompressDialog.cs
@@ -24,8 +24,6 @@ namespace UltimateArchiverZ
 
 		private global::Gtk.Table table3;
 
-		private global::Stetic.Custom custom2;
-
 		private global::Gtk.CheckButton passowrdCheckbox;
 
 		private global::Gtk.Entry passwordEntry;
@@ -45,7 +43,6 @@ namespace UltimateArchiverZ
 			global::Stetic.Gui.Initialize(this);
 			// Widget UltimateArchiverZ.CompressDialog
 			this.Name = "UltimateArchiverZ.CompressDialog";
-			this.Title = global::Mono.Unix.Catalog.GetString("window1");
 			this.WindowPosition = ((global::Gtk.WindowPosition)(4));
 			// Internal child UltimateArchiverZ.CompressDialog.VBox
 			global::Gtk.VBox w1 = this.VBox;
@@ -133,17 +130,6 @@ namespace UltimateArchiverZ
 			this.table3.RowSpacing = ((uint)(6));
 			this.table3.ColumnSpacing = ((uint)(6));
 			// Container child table3.Gtk.Table+TableChild
-			this.custom2 = new global::Stetic.Custom();
-			this.custom2.Name = "custom2";
-			this.custom2.Int1 = 0;
-			this.custom2.Int2 = 0;
-			this.table3.Add(this.custom2);
-			global::Gtk.Table.TableChild w10 = ((global::Gtk.Table.TableChild)(this.table3[this.custom2]));
-			w10.LeftAttach = ((uint)(2));
-			w10.RightAttach = ((uint)(3));
-			w10.XOptions = ((global::Gtk.AttachOptions)(4));
-			w10.YOptions = ((global::Gtk.AttachOptions)(4));
-			// Container child table3.Gtk.Table+TableChild
 			this.passowrdCheckbox = new global::Gtk.CheckButton();
 			this.passowrdCheckbox.CanFocus = true;
 			this.passowrdCheckbox.Name = "passowrdCheckbox";
@@ -151,8 +137,8 @@ namespace UltimateArchiverZ
 			this.passowrdCheckbox.DrawIndicator = true;
 			this.passowrdCheckbox.UseUnderline = true;
 			this.table3.Add(this.passowrdCheckbox);
-			global::Gtk.Table.TableChild w11 = ((global::Gtk.Table.TableChild)(this.table3[this.passowrdCheckbox]));
-			w11.YOptions = ((global::Gtk.AttachOptions)(4));
+			global::Gtk.Table.TableChild w10 = ((global::Gtk.Table.TableChild)(this.table3[this.passowrdCheckbox]));
+			w10.YOptions = ((global::Gtk.AttachOptions)(4));
 			// Container child table3.Gtk.Table+TableChild
 			this.passwordEntry = new global::Gtk.Entry();
 			this.passwordEntry.Sensitive = false;
@@ -162,10 +148,10 @@ namespace UltimateArchiverZ
 			this.passwordEntry.Visibility = false;
 			this.passwordEntry.InvisibleChar = '•';
 			this.table3.Add(this.passwordEntry);
-			global::Gtk.Table.TableChild w12 = ((global::Gtk.Table.TableChild)(this.table3[this.passwordEntry]));
-			w12.LeftAttach = ((uint)(1));
-			w12.RightAttach = ((uint)(2));
-			w12.YOptions = ((global::Gtk.AttachOptions)(4));
+			global::Gtk.Table.TableChild w11 = ((global::Gtk.Table.TableChild)(this.table3[this.passwordEntry]));
+			w11.LeftAttach = ((uint)(1));
+			w11.RightAttach = ((uint)(2));
+			w11.YOptions = ((global::Gtk.AttachOptions)(4));
 			// Container child table3.Gtk.Table+TableChild
 			this.splitCheckbox = new global::Gtk.CheckButton();
 			this.splitCheckbox.CanFocus = true;
@@ -174,10 +160,10 @@ namespace UltimateArchiverZ
 			this.splitCheckbox.DrawIndicator = true;
 			this.splitCheckbox.UseUnderline = true;
 			this.table3.Add(this.splitCheckbox);
-			global::Gtk.Table.TableChild w13 = ((global::Gtk.Table.TableChild)(this.table3[this.splitCheckbox]));
-			w13.TopAttach = ((uint)(1));
-			w13.BottomAttach = ((uint)(2));
-			w13.YOptions = ((global::Gtk.AttachOptions)(4));
+			global::Gtk.Table.TableChild w12 = ((global::Gtk.Table.TableChild)(this.table3[this.splitCheckbox]));
+			w12.TopAttach = ((uint)(1));
+			w12.BottomAttach = ((uint)(2));
+			w12.YOptions = ((global::Gtk.AttachOptions)(4));
 			// Container child table3.Gtk.Table+TableChild
 			this.splitSizeComboBox = global::Gtk.ComboBox.NewText();
 			this.splitSizeComboBox.AppendText(global::Mono.Unix.Catalog.GetString("b"));
@@ -188,13 +174,13 @@ namespace UltimateArchiverZ
 			this.splitSizeComboBox.Name = "splitSizeComboBox";
 			this.splitSizeComboBox.Active = 0;
 			this.table3.Add(this.splitSizeComboBox);
-			global::Gtk.Table.TableChild w14 = ((global::Gtk.Table.TableChild)(this.table3[this.splitSizeComboBox]));
-			w14.TopAttach = ((uint)(1));
-			w14.BottomAttach = ((uint)(2));
-			w14.LeftAttach = ((uint)(2));
-			w14.RightAttach = ((uint)(3));
-			w14.XOptions = ((global::Gtk.AttachOptions)(4));
-			w14.YOptions = ((global::Gtk.AttachOptions)(4));
+			global::Gtk.Table.TableChild w13 = ((global::Gtk.Table.TableChild)(this.table3[this.splitSizeComboBox]));
+			w13.TopAttach = ((uint)(1));
+			w13.BottomAttach = ((uint)(2));
+			w13.LeftAttach = ((uint)(2));
+			w13.RightAttach = ((uint)(3));
+			w13.XOptions = ((global::Gtk.AttachOptions)(4));
+			w13.YOptions = ((global::Gtk.AttachOptions)(4));
 			// Container child table3.Gtk.Table+TableChild
 			this.splitSizeEntry = new global::Gtk.Entry();
 			this.splitSizeEntry.Sensitive = false;
@@ -203,26 +189,26 @@ namespace UltimateArchiverZ
 			this.splitSizeEntry.IsEditable = true;
 			this.splitSizeEntry.InvisibleChar = '•';
 			this.table3.Add(this.splitSizeEntry);
-			global::Gtk.Table.TableChild w15 = ((global::Gtk.Table.TableChild)(this.table3[this.splitSizeEntry]));
-			w15.TopAttach = ((uint)(1));
-			w15.BottomAttach = ((uint)(2));
-			w15.LeftAttach = ((uint)(1));
-			w15.RightAttach = ((uint)(2));
-			w15.YOptions = ((global::Gtk.AttachOptions)(4));
+			global::Gtk.Table.TableChild w14 = ((global::Gtk.Table.TableChild)(this.table3[this.splitSizeEntry]));
+			w14.TopAttach = ((uint)(1));
+			w14.BottomAttach = ((uint)(2));
+			w14.LeftAttach = ((uint)(1));
+			w14.RightAttach = ((uint)(2));
+			w14.YOptions = ((global::Gtk.AttachOptions)(4));
 			w9.Add(this.table3);
 			this.scrolledwindow1.Add(w9);
 			this.vbox2.Add(this.scrolledwindow1);
-			global::Gtk.Box.BoxChild w18 = ((global::Gtk.Box.BoxChild)(this.vbox2[this.scrolledwindow1]));
-			w18.Position = 2;
+			global::Gtk.Box.BoxChild w17 = ((global::Gtk.Box.BoxChild)(this.vbox2[this.scrolledwindow1]));
+			w17.Position = 2;
 			w1.Add(this.vbox2);
-			global::Gtk.Box.BoxChild w19 = ((global::Gtk.Box.BoxChild)(w1[this.vbox2]));
-			w19.Position = 0;
+			global::Gtk.Box.BoxChild w18 = ((global::Gtk.Box.BoxChild)(w1[this.vbox2]));
+			w18.Position = 0;
 			// Internal child UltimateArchiverZ.CompressDialog.ActionArea
-			global::Gtk.HButtonBox w20 = this.ActionArea;
-			w20.Name = "dialog1_ActionArea";
-			w20.Spacing = 10;
-			w20.BorderWidth = ((uint)(5));
-			w20.LayoutStyle = ((global::Gtk.ButtonBoxStyle)(4));
+			global::Gtk.HButtonBox w19 = this.ActionArea;
+			w19.Name = "dialog1_ActionArea";
+			w19.Spacing = 10;
+			w19.BorderWidth = ((uint)(5));
+			w19.LayoutStyle = ((global::Gtk.ButtonBoxStyle)(4));
 			// Container child dialog1_ActionArea.Gtk.ButtonBox+ButtonBoxChild
 			this.buttonCancel = new global::Gtk.Button();
 			this.buttonCancel.CanDefault = true;
@@ -232,9 +218,9 @@ namespace UltimateArchiverZ
 			this.buttonCancel.UseUnderline = true;
 			this.buttonCancel.Label = "gtk-cancel";
 			this.AddActionWidget(this.buttonCancel, -6);
-			global::Gtk.ButtonBox.ButtonBoxChild w21 = ((global::Gtk.ButtonBox.ButtonBoxChild)(w20[this.buttonCancel]));
-			w21.Expand = false;
-			w21.Fill = false;
+			global::Gtk.ButtonBox.ButtonBoxChild w20 = ((global::Gtk.ButtonBox.ButtonBoxChild)(w19[this.buttonCancel]));
+			w20.Expand = false;
+			w20.Fill = false;
 			// Container child dialog1_ActionArea.Gtk.ButtonBox+ButtonBoxChild
 			this.buttonOk = new global::Gtk.Button();
 			this.buttonOk.CanDefault = true;
@@ -243,10 +229,10 @@ namespace UltimateArchiverZ
 			this.buttonOk.UseUnderline = true;
 			this.buttonOk.Label = global::Mono.Unix.Catalog.GetString("Compress");
 			this.AddActionWidget(this.buttonOk, -5);
-			global::Gtk.ButtonBox.ButtonBoxChild w22 = ((global::Gtk.ButtonBox.ButtonBoxChild)(w20[this.buttonOk]));
-			w22.Position = 1;
-			w22.Expand = false;
-			w22.Fill = false;
+			global::Gtk.ButtonBox.ButtonBoxChild w21 = ((global::Gtk.ButtonBox.ButtonBoxChild)(w19[this.buttonOk]));
+			w21.Position = 1;
+			w21.Expand = false;
+			w21.Fill = false;
 			if ((this.Child != null))
 			{
 				this.Child.ShowAll();
